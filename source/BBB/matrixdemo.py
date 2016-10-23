6ab342a0085320a5accf4e3c128be83812a2f5ce
adam <jezek.adamek@gmail.com>
2016-10-23 14:32:36 +0200

    ADSFJK

--- source/BBB/matrixdemo.py
+++ source/BBB/matrixdemo.py
@@ -5,6 +5,7 @@ class MatrixDemo(threading.Thread):
     WIDTH = 15
     HEIGHT = 9
     leds = []
+    startTime = 0
 
     def __init__(self):
         # Call Thread class's constructor
@@ -13,6 +14,7 @@ class MatrixDemo(threading.Thread):
         # game is not running from start
         self.running = False
         self.leds = [(0,0,0) for x in range(self.WIDTH*self.HEIGHT)]
+        self.startTime = time.time()
 
     def prepare(self, svetelny_panel, wiimote1, wiimote2, infrapen):
         self.svetelny_panel = svetelny_panel
@@ -25,6 +27,7 @@ class MatrixDemo(threading.Thread):
         self.running = True
         self.leds = [(0, 0, 0) for x in range(self.WIDTH * self.HEIGHT)]
         print("Starting the demo matrix")
+        self.startTime = time.time()
         # This starts the parallel thread (self.run() method is called)
         self.start()
 
@@ -36,7 +39,8 @@ class MatrixDemo(threading.Thread):
         self.gameloop()
 
     def gameloop(self):
-        ms = (time.time() / 1000)
+        ms = ((time.time()-self.startTime) * 1000)
+        #print ms
         yHueDelta32 = int((math.cos(ms * (27 / 1)) * (350 / self.WIDTH)))
         xHueDelta32 = int(math.cos(ms * (39 / 1)) * (310 / self.HEIGHT))
         self.draw_one_frame(ms / 65536, yHueDelta32 / 32768, xHueDelta32 / 32768);
@@ -50,31 +54,41 @@ class MatrixDemo(threading.Thread):
             pixelHue = lineStartHue
             for x in range(self.WIDTH):
                 pixelHue += xHueDelta8
-                self.leds[self.XY(x, y)] = colorsys.hsv_to_rgb(pixelHue, 255, 255)
+                # print "pixelHue",pixelHue*8
+                self.leds[self.XY(x, y)] = self.hsv_to_rgb(pixelHue * 8, 100, 100)
+                # print self.leds[self.XY(x, y)]
 
     def XY(self, x, y):
-        i = 0
-        i = (y * self.HEIGHT) + x
-        return i
+        return (y * self.HEIGHT) + x
 
     def build_matrix(self):
-
-        matrix = [[colors.BLACK for x in range(self.WIDTH)] for y in range(self.HEIGHT)]
+        matrix = [[(0, 0, 0) for x in range(self.WIDTH)] for y in range(self.HEIGHT)]
         for x in range(self.WIDTH):
             for y in range(self.HEIGHT):
                 rgbhex = ""
                 try:
+                    # print "ledXY", (self.leds[self.XY(x, y)])
                     rgbhex = self.rgb_to_hex(self.leds[self.XY(x, y)])
                     matrix[y][x] = rgbhex[1:]
-                    print(rgbhex)
-                    print(self.leds[self.XY(x, y)])
+                    # print "rgbhex", rgbhex,
+                    # print(self.leds[self.XY(x, y)])
                 except:
-                    print(rgbhex+str(x)+","+str(y)+","+str(self.XY(x,y))+","+str(len(self.leds)))
+                    print("outofrange" + rgbhex + str(x) + "," + str(y) + "," + str(self.XY(x, y)) + "," + str(
+                        len(self.leds)))
 
         return matrix
 
     def rgb_to_hex(self, rgb):
         return '#%02x%02x%02x' % rgb
+
+    def hsv_to_rgb(self, h, s, v):
+        if h < 0:
+            h = 360 - h
+        # print "htr", h, s, v
+        color = colorsys.hsv_to_rgb(h / 360.0, s / 100.0, v / 100.0)
+        converted = (int(round(color[0] * 255)), int(round(color[1] * 255)), int(round(color[2] * 255)))
+        # print "htr res; ",converted
+        return converted
 
     def show(self):
         matrix = self.build_matrix()
